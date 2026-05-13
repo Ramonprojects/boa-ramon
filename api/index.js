592b0b445316dde086f780366c07db3e05e2778f
Ramonprojects <adm@ramonmago.com>
2026-05-13 12:42:45 -0300

    Update redirect URL in index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=NF4J2KXLUFC&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=D6NL1LKQCU3&afp=bd' });
     res.end();
   }
