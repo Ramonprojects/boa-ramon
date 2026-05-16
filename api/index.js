5234155076ca0c049c4a206cff5290507a7c4790
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-16 11:55:29 -0300

    chore: update shareCode to R0GH6Y99FG4

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=07ES0D7NZXR&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=R0GH6Y99FG4&afp=bd' });
     res.end();
   }
