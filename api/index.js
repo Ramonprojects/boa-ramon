d60fa8d955e7c39b63ea86c55c9f444f0281d226
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-27 10:09:39 -0300

    chore: update shareCode to UXQPT0BIQBC

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=PEXWMMG25SM&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=UXQPT0BIQBC&afp=bd' });
     res.end();
   }
