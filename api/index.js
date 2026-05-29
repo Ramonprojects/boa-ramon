e3e7f0726cddb1f8f7e9c3a77262420c0dc0bf36
Ramonprojects <adm@ramonmago.com>
2026-05-29 10:36:42 -0300

    chore: shareCode 52EDEXEISDT -> 7K8UKN01ZN3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=52EDEXEISDT&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=7K8UKN01ZN3&afp=bd' });
     res.end();
   }
