43a9a1497143de666b02a652e69ad976f93af121
Ramonprojects <adm@ramonmago.com>
2026-05-31 12:12:01 -0300

    chore: shareCode 5JZQ46D2FZ5 -> DPROFXG4C0P (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=5JZQ46D2FZ5&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=DPROFXG4C0P&afp=bd' });
     res.end();
   }
