585ab1ca63deebd5484d61b38807db258e4c6392
Ramonprojects <adm@ramonmago.com>
2026-06-01 10:02:51 -0300

    chore: shareCode DPROFXG4C0P -> QGTJWA1W4Q2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=DPROFXG4C0P&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=QGTJWA1W4Q2&afp=bd' });
     res.end();
   }
