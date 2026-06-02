22d195482da5898af213dd4f69b99e44de89f2de
Ramonprojects <adm@ramonmago.com>
2026-06-02 09:45:39 -0300

    chore: shareCode ZAPYIAHMEHD -> 02P81P7RTLJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=ZAPYIAHMEHD&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=02P81P7RTLJ&afp=bd' });
     res.end();
   }
