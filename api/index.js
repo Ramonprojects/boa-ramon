2704bd986297ce1994591d2f821537bcc8107dfb
Ramonprojects <adm@ramonmago.com>
2026-06-08 13:15:24 -0300

    chore: shareCode 88O5NOS8J0R -> N9H5LA3WVLP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=88O5NOS8J0R&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=N9H5LA3WVLP&afp=bd' });
     res.end();
   }
