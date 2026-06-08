0f2941be6fac9c6b753eca3ba66983a5ee0e1f89
Ramonprojects <adm@ramonmago.com>
2026-06-08 18:47:08 -0300

    chore: shareCode N9H5LA3WVLP -> I0XF7UHDCKU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=N9H5LA3WVLP&afp=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=I0XF7UHDCKU&afp=bd' });
     res.end();
   }
