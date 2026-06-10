5df94b18ce047f1c7e72a4f6029673b4dac06b83
Ramonprojects <adm@ramonmago.com>
2026-06-10 10:24:58 -0300

    chore: shareCode 5QOUKVAF0DY -> 7U53NIIUPSO (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=5QOUKVAF0DY&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=7U53NIIUPSO&afp1=bd' });
     res.end();
   }
