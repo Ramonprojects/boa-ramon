e0290001a5e2de071bd5114ab9f0b75aa1ec5081
Ramonprojects <adm@ramonmago.com>
2026-06-20 11:47:42 -0300

    chore: shareCode N0Z5FU5Q08D -> TD0TO155QUI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=N0Z5FU5Q08D&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=TD0TO155QUI&afp1=bd' });
     res.end();
   }
