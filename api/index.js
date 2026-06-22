ab7478692e4b451fe700872e5b052e6f8fff373c
Ramonprojects <adm@ramonmago.com>
2026-06-22 12:47:53 -0300

    chore: shareCode DS11U2IRUEL -> GLE2EN1NPUQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=DS11U2IRUEL&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=GLE2EN1NPUQ&afp1=bd' });
     res.end();
   }
