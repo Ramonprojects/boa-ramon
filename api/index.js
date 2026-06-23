a7ff5d00e98a9cd4e25e483ca0579f3d5ea272bb
Ramonprojects <adm@ramonmago.com>
2026-06-23 11:31:42 -0300

    chore: shareCode GLE2EN1NPUQ -> UMJ4CCYFT2N (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=GLE2EN1NPUQ&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=UMJ4CCYFT2N&afp1=bd' });
     res.end();
   }
