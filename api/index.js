bd5d1542334bfc6653f0da491ea734c1d425040a
Ramonprojects <adm@ramonmago.com>
2026-06-24 13:15:35 -0300

    chore: shareCode UMJ4CCYFT2N -> QGYRNB8ZN90 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=UMJ4CCYFT2N&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=QGYRNB8ZN90&afp1=bd' });
     res.end();
   }
