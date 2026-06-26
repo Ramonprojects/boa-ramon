64b1e9e1363c5523aad85aff534ca235e01ad706
Ramonprojects <adm@ramonmago.com>
2026-06-26 15:18:37 -0300

    chore: shareCode CTP4F62L0D8 -> WK0IFJXETEG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=CTP4F62L0D8&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=WK0IFJXETEG&afp1=bd' });
     res.end();
   }
