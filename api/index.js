576b134514be51e8808f2b7b5c7caec75069ba44
Ramonprojects <adm@ramonmago.com>
2026-06-29 10:41:59 -0300

    chore: shareCode GEQ441M7VI7 -> 7LDSZ0O7VQR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=GEQ441M7VI7&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=7LDSZ0O7VQR&afp1=bd' });
     res.end();
   }
