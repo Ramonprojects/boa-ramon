6ed0d6f7b30084dc9e5311b215c7e62d139a6e96
Ramonprojects <adm@ramonmago.com>
2026-06-30 09:45:36 -0300

    chore: shareCode 7LDSZ0O7VQR -> NLGYZ3QXTAS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=7LDSZ0O7VQR&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=NLGYZ3QXTAS&afp1=bd' });
     res.end();
   }
