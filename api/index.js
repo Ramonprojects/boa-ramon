bf5332ec47b99e78e326674ad133832200235d48
Ramonprojects <adm@ramonmago.com>
2026-07-06 10:42:42 -0300

    chore: shareCode HTUKWDIJDYR -> I1S0L2E7M7O (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=HTUKWDIJDYR&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=I1S0L2E7M7O&afp1=bd' });
     res.end();
   }
