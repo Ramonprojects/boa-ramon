1acd5141a838c147589094f1d0726db6c562e84d
Ramonprojects <adm@ramonmago.com>
2026-07-10 09:35:50 -0300

    chore: shareCode R12G511RP8M -> KOPC2EOQCSQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=R12G511RP8M&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=KOPC2EOQCSQ&afp1=bd' });
     res.end();
   }
