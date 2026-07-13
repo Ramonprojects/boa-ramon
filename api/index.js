2588667bc3fc65c12595681f504efcc17beff726
Ramonprojects <adm@ramonmago.com>
2026-07-13 11:59:20 -0300

    chore: shareCode K8SPCYF3L6R -> 3QCTXGYQMG3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=K8SPCYF3L6R&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=3QCTXGYQMG3&afp1=bd' });
     res.end();
   }
