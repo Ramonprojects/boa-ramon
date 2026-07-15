6b30325bb274f894b4fab89d2efd46848912ed80
Ramonprojects <adm@ramonmago.com>
2026-07-15 11:12:23 -0300

    chore: shareCode FQ4WOEYM116 -> L97M81J5N8L (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=FQ4WOEYM116&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=L97M81J5N8L&afp1=bd' });
     res.end();
   }
