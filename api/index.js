0043f65905a5a2e4abb3ee6639848f32dd678c5b
Ramonprojects <adm@ramonmago.com>
2026-07-27 13:26:13 -0300

    chore: shareCode H042F9DGAJW -> GLKEQTVNN4R (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=H042F9DGAJW&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=GLKEQTVNN4R&afp1=bd' });
     res.end();
   }
