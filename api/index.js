cba7ed08f6705626c633a4fbad705ea755b83814
Ramonprojects <adm@ramonmago.com>
2026-08-02 11:27:53 -0300

    chore: shareCode TLE9CAF1M6Y -> BICQJ0QS43R (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=TLE9CAF1M6Y&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=BICQJ0QS43R&afp1=bd' });
     res.end();
   }
