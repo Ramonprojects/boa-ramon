5845d4a5eee06dcaab6b05d3c4cc47c8e604e4b3
Ramonprojects <adm@ramonmago.com>
2026-08-07 10:49:54 -0300

    chore: shareCode 8BCFMOKQC6H -> SEDT3CWIUNQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=8BCFMOKQC6H&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=SEDT3CWIUNQ&afp1=bd' });
     res.end();
   }
