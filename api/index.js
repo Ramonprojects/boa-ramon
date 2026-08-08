b048ed72baaaa65d7c337bc2878b101d6bf2d2a2
Ramonprojects <adm@ramonmago.com>
2026-08-08 11:54:47 -0300

    chore: shareCode SEDT3CWIUNQ -> VM8L71O0V5D (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=SEDT3CWIUNQ&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VM8L71O0V5D&afp1=bd' });
     res.end();
   }
