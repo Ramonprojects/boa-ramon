a5a5a35a94d1a93800111102439212acbe3cfee7
Ramonprojects <adm@ramonmago.com>
2026-08-16 10:57:38 -0300

    chore: shareCode LQXXA2GNXG7 -> 1K91U0R15X2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=LQXXA2GNXG7&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=1K91U0R15X2&afp1=bd' });
     res.end();
   }
