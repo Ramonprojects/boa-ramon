f52df1d0cd4c382d3a34480556341f7aa4e31d54
Ramonprojects <adm@ramonmago.com>
2026-08-22 11:07:42 -0300

    chore: shareCode 6PNGK160F3L -> 0KTOTH61ZVK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=6PNGK160F3L&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=0KTOTH61ZVK&afp1=bd' });
     res.end();
   }
