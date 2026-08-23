29ed733e197af37d5ccfb5e69cc8576decc2dac5
Ramonprojects <adm@ramonmago.com>
2026-08-23 10:38:12 -0300

    chore: shareCode 0KTOTH61ZVK -> CVE5PKAHYM6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=0KTOTH61ZVK&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=CVE5PKAHYM6&afp1=bd' });
     res.end();
   }
