96b1e583484fc6e8ae6011fd3db730ababcbbe69
Ramonprojects <adm@ramonmago.com>
2026-08-28 10:55:31 -0300

    chore: shareCode B0YLIPAU2TN -> Y569TDQ3MP9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=B0YLIPAU2TN&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=Y569TDQ3MP9&afp1=bd' });
     res.end();
   }
