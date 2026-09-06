91a937c708ba615b424db517e12a7f4604076a1e
Ramonprojects <adm@ramonmago.com>
2026-09-06 10:30:36 -0300

    chore: shareCode KAJD4FFIK2K -> B5VKSV5COJI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=KAJD4FFIK2K&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=B5VKSV5COJI&afp1=bd' });
     res.end();
   }
