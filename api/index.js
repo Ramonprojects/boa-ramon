447ee0d9fc3f66efb8ab6d282e2b8b01d60fdee4
Ramonprojects <adm@ramonmago.com>
2026-09-08 12:39:08 -0300

    chore: shareCode LSQV2YG3SR4 -> 6F6ZI6ND2MV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=LSQV2YG3SR4&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=6F6ZI6ND2MV&afp1=bd' });
     res.end();
   }
