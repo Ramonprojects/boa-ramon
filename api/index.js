df24577f31b0c92ed4ffa41f5802bb82997bb0ce
Ramonprojects <adm@ramonmago.com>
2026-09-10 10:59:48 -0300

    chore: shareCode FFW3OTL3Z1J -> QXOE2A4H18H (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=FFW3OTL3Z1J&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=QXOE2A4H18H&afp1=bd' });
     res.end();
   }
