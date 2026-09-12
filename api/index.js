e11347ee1d0cd68020041f50cb70a05ce612c6bc
Ramonprojects <adm@ramonmago.com>
2026-09-12 16:10:18 -0300

    chore: shareCode PXQJ30TQ8TX -> A8X4AMJMD3G (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=PXQJ30TQ8TX&afp1=bd' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=A8X4AMJMD3G&afp1=bd' });
     res.end();
   }
